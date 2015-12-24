c5c95bf566d903c66336ff5ccb81aa82ccaf3ec1
Jesse J. Anderson <jessejanderson@gmail.com>
2015-12-24 15:37:52 -0800

    add feature for all planets

--- lib/rocketry.ex
+++ lib/rocketry.ex
@@ -1,29 +1,60 @@
-defmodule Calcs do
-  def rounded_to_nearest_tenth(val) do
-    Float.ceil(val, 1)
-  end
-
-  def convert_to_km(velocity) do
-    velocity / 1000
-  end
-end
-
 defmodule Physics.Rocketry do
-  def escape_velocity(:earth) do
-    %{mass: 5.972e24, radius: 6.371e6}
-      |> escape_velocity
-  end
 
+  import Calcs
+  import Physics.Laws
+  import Planets
+
+  def escape_velocity(:earth), do: escape_velocity(earth)
+  def escape_velocity(:moon),  do: escape_velocity(moon)
+  def escape_velocity(:mars),  do: escape_velocity(mars)
   def escape_velocity(planet) when is_map(planet) do
     planet
-      |> calculate_escape
-      |> Calcs.convert_to_km
-      |> Calcs.rounded_to_nearest_tenth
+    |> calculate_escape
+    |> to_km
+    |> to_nearest_tenth
+  end
+
+  def orbital_speed(height),        do: orbital_speed(earth, height)
+  def orbital_speed(planet, height) do
+    newtons_gravitational_constant * planet.mass / orbital_radius(height)
+    |> square_root
+  end
+
+  def orbital_acceleration(height),         do: orbital_acceleration(earth, height)
+  def orbital_acceleration(:earth, height), do: orbital_acceleration(earth, height)
+  def orbital_acceleration(:moon, height),  do: orbital_acceleration(moon, height)
+  def orbital_acceleration(:mars, height),  do: orbital_acceleration(mars, height)
+  def orbital_acceleration(planet, height)  do
+    (orbital_speed(planet, height) |> squared) / orbital_radius(planet, height)
+  end
+
+  # T = √(4 * pi^2 * R^3) / G * M
+  def orbital_term(height),         do: orbital_term(earth, height)
+  def orbital_term(:earth, height), do: orbital_term(earth, height)
+  def orbital_term(:moon, height),  do: orbital_term(moon, height)
+  def orbital_term(:mars, height),  do: orbital_term(mars, height)
+  def orbital_term(planet, height)  do
+    4 * (:math.pi |> squared) * (orbital_radius(planet, height) |> cubed) / (newtons_gravitational_constant * planet.mass)
+    |> square_root
+    |> seconds_to_hours
+  end
+
+  # EXTRA CREDIT
+  # (doesn't seem to work?)
+  # R = ³√G * M * T^2 / 4 * pi^2
+  def calculate_time_by_term(hours),        do: calculate_time_by_term(earth, hours)
+  def calculate_time_by_term(planet, hours) do
+    cube_root(newtons_gravitational_constant) * planet.mass * (hours |> squared) / 4 * (:math.pi |> squared)
+    |> orbital_radius
+  end
+
+  def orbital_radius(height), do: orbital_radius(earth, height)
+  def orbital_radius(planet, height) do
+    planet.radius + (height |> to_m)
   end
 
   defp calculate_escape(%{mass: mass, radius: radius}) do
-    newtons_constant = 6.67e-11
-    2 * newtons_constant * mass / radius
-      |> :math.sqrt
+    2 * newtons_gravitational_constant * mass / radius
+    |> square_root
   end
 end
--- test/physics_test.exs
+++ test/physics_test.exs
@@ -10,15 +10,19 @@ defmodule PhysicsTest do
     assert Physics.Rocketry.escape_velocity(:earth) == 11.2
   end
 
+  test "Moon escape velocity is 2.4" do
+    assert Physics.Rocketry.escape_velocity(:moon) == 2.4
+  end
+
+  test "Earth escape velocity is 5.1" do
+    assert Physics.Rocketry.escape_velocity(:mars) == 5.1
+  end
+
   test "Earth (explicit) escape velocity is 11.2" do
     assert Physics.Rocketry.escape_velocity(%{mass: 5.972e24, radius: 6.371e6}) == 11.2
   end
 
-  test "Moon escape velocity is 2.4" do
-    assert Physics.Rocketry.escape_velocity(%{mass: 7.35e22, radius: 1.738e6}) == 2.4
-  end
-
-  test "Mars escape velocity is 5.1" do
-    assert Physics.Rocketry.escape_velocity(%{mass: 6.39e23, radius: 3.4e6}) == 5.1
+  test "Orbital term for height of 100km is 1.5" do
+    assert Physics.Rocketry.orbital_term(100) == 1.5
   end
 end
